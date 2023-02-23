39adf14420097902fa0563922ce6f4d8b7aceb60
Burin Choomnuan <burin.choomnuan@gmail.com>
2023-02-23 12:16:41 -0500

    Add simple test code

--- src/spike_floobits/core.clj
+++ src/spike_floobits/core.clj
@@ -4,3 +4,20 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(defn add-one [x]
+  (+ 1 x))
+
+(defn add-two [x]
+  (+ 2 x))
+
+(comment
+  (add-one 3)
+
+  (map add-two (range 10))  ;;=> (2 3 4 5 6 7 8 9 10 11)
+  (map add-one [1 2 3 4 5]) ;;=> (2 3 4 5 6)
+
+  nil)
+
+;; NOTE: from Emacs try M-x floobits-clear-highlights
+nil
